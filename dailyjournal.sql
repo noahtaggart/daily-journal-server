29834af662f7ae3eb48a9b2aeff2101cca9a3404
Noah Taggart <noahtag@gmail.com>
2022-04-11 13:31:32 -0500

    Including Mood on Entry

--- dailyjournal.sql
+++ dailyjournal.sql
@@ -22,13 +22,26 @@ CREATE TABLE `Moods` (
 INSERT INTO `Entries` VALUES (null, "Ate some ice cream today", 04112022, 1, 2);
 INSERT INTO `Entries` VALUES (null, "Reorganized bed room. Bought new vacuum after last one exploded", 04102022, 2, 3);
 INSERT INTO `Entries` VALUES (null, "Got headshots taken and got some dope bibimbap", 04092022, 3, 1);
+INSERT INTO `Moods` VALUES (null, "Happy");
+INSERT INTO `Moods` VALUES (null, "Sad");
+INSERT INTO `Moods` VALUES (null, "Content");
 
-
--- SELECT *
--- FROM Entries e
--- WHERE e.journalEntry LIKE "%ate%" 
 
 SELECT *
 FROM Entries e
-WHERE CONTAINS(e.journalEntry,"ate")
+WHERE e.journalEntry LIKE "%ate%" 
 
+SELECT *
+FROM Entries e
+WHERE e.journalEntry CONTAINS "ate"
+
+   SELECT
+            e.id,
+            e.journalEntry,
+            e.date,
+            e.userId,
+            e.moodId,
+            m.name mood
+        FROM Entries e
+        Join Moods m
+            ON m.id = e.moodId
---
+++ models/mood.py
@@ -0,0 +1,4 @@
+class Mood():
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
